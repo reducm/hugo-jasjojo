5f2cabf20c8eb93a1791020c56a626329d50e051
Jas <jas@jasjojo.com>
2026-02-25 11:17:43 +0800

    部署更新

---
+++ content/posts/zhihu_hot_list_2025-02-25.py
@@ -0,0 +1,185 @@
+# 知乎热榜数据 - 2025-02-25
+# 已获取前20个问题
+
+hot_list_data = [
+    {
+        "rank": 1,
+        "id": "2009430555993846528",
+        "title": "如何评价起点万订作者过年回家被母亲批评不务正业，要求其进厂打工？起点万订属于什么水平？能养活自己吗？",
+        "url": "https://www.zhihu.com/question/2009430555993846528",
+        "heat": "1055万热度",
+        "answer_count": 194,
+        "follower_count": 490
+    },
+    {
+        "rank": 2,
+        "id": "2009733369261466626",
+        "title": "如何评价懂车帝发布的视频「小米纽北遭法拉利保时捷围剿？冠军车手为何沉默」？",
+        "url": "https://www.zhihu.com/question/2009733369261466626",
+        "heat": "604万热度",
+        "answer_count": 121,
+        "follower_count": 248
+    },
+    {
+        "rank": 3,
+        "id": "1998323519126249765",
+        "title": "为何《飞驰人生 3》能在两年以内完成制作并上映，而《哪吒》等其他电影的第三部却需要三年以上？",
+        "url": "https://www.zhihu.com/question/1998323519126249765",
+        "heat": "314万热度",
+        "answer_count": 66,
+        "follower_count": 118
+    },
+    {
+        "rank": 4,
+        "id": "2009652656289833304",
+        "title": "尼格买提晒家宴，8 个菜没有一片绿叶，网友调侃「碳水盛宴」，新疆人真的不爱吃蔬菜吗？为什么？",
+        "url": "https://www.zhihu.com/question/2009652656289833304",
+        "heat": "218万热度",
+        "answer_count": 33,
+        "follower_count": 70
+    },
+    {
+        "rank": 5,
+        "id": "2006631956604003126",
+        "title": "字节跳动超 400 亿元出售沐瞳科技，对其游戏业务意味着什么？",
+        "url": "https://www.zhihu.com/question/2006631956604003126",
+        "heat": "213万热度",
+        "answer_count": 52,
+        "follower_count": 288
+    },
+    {
+        "rank": 6,
+        "id": "2009896910291428421",
+        "title": "平顶山再通报打人事件，涉事夫妻涉嫌「寻衅滋事罪」被批捕，哪些信息值得关注？",
+        "url": "https://www.zhihu.com/question/2009896910291428421",
+        "heat": "205万热度",
+        "answer_count": 70,
+        "follower_count": 122
+    },
+    {
+        "rank": 7,
+        "id": "644399781",
+        "title": "史书记载都是权谋和政治斗争，计谋怎样实施的？为啥没人说，是看不懂么？",
+        "url": "https://www.zhihu.com/question/644399781",
+        "heat": "196万热度",
+        "answer_count": 24,
+        "follower_count": 177
+    },
+    {
+        "rank": 8,
+        "id": "2009646141273301871",
+        "title": "巴拿马政府强行接管李嘉诚旗下港口，长和回应「相关行动不合法」，如何解读？是否违背了国际贸易基本规则？",
+        "url": "https://www.zhihu.com/question/2009646141273301871",
+        "heat": "181万热度",
+        "answer_count": 16,
+        "follower_count": 46
+    },
+    {
+        "rank": 9,
+        "id": "2009667280913524518",
+        "title": "新年BBA车价大降，宝马 7 系降价约 27 万，曾经加价提车的BBA，如今为何要靠「骨折价」抢市场？",
+        "url": "https://www.zhihu.com/question/2009667280913524518",
+        "heat": "168万热度",
+        "answer_count": 39,
+        "follower_count": 58
+    },
+    {
+        "rank": 10,
+        "id": "2008671520495522709",
+        "title": "对于普通人来说，"高薪大厂工作十年后考上编制"是不是顶配的幸福感？",
+        "url": "https://www.zhihu.com/question/2008671520495522709",
+        "heat": "163万热度",
+        "answer_count": 75,
+        "follower_count": 386
+    },
+    {
+        "rank": 11,
+        "id": "2009346634488701235",
+        "title": "德国总理默茨将访华，会对中德关系带来哪些影响？",
+        "url": "https://www.zhihu.com/question/2009346634488701235",
+        "heat": "153万热度",
+        "answer_count": 45,
+        "follower_count": 108
+    },
+    {
+        "rank": 12,
+        "id": "2004605278973363722",
+        "title": "为什么《红楼梦》几乎没有正面"宅斗"，却仍然是中国最残酷的权力叙事？",
+        "url": "https://www.zhihu.com/question/2004605278973363722",
+        "heat": "143万热度",
+        "answer_count": 43,
+        "follower_count": 189
+    },
+    {
+        "rank": 13,
+        "id": "2009410282296989634",
+        "title": "如何看待《明日方舟：终末地》中，莱万汀的复燃天赋90%减伤实为10%减伤？",
+        "url": "https://www.zhihu.com/question/2009410282296989634",
+        "heat": "128万热度",
+        "answer_count": 22,
+        "follower_count": 27
+    },
+    {
+        "rank": 14,
+        "id": "2009528155782805337",
+        "title": "韩寒是不是中国竞技体育史上最特殊的案例？他的经历有什么特殊的地方？",
+        "url": "https://www.zhihu.com/question/2009528155782805337",
+        "heat": "111万热度",
+        "answer_count": 19,
+        "follower_count": 36
+    },
+    {
+        "rank": 15,
+        "id": "2009581955323224341",
+        "title": "男篮将战日本，在西乙打球的余嘉豪转机5次历时30个小时万里驰援，如何评价余嘉豪这一举动？",
+        "url": "https://www.zhihu.com/question/2009581955323224341",
+        "heat": "102万热度",
+        "answer_count": 20,
+        "follower_count": 22
+    },
+    {
+        "rank": 16,
+        "id": "2009569700791752615",
+        "title": "商务部宣布将 20 家日本实体列入出口管制管控名单，涉及哪些领域？将给日本带来哪些影响？",
+        "url": "https://www.zhihu.com/question/2009569700791752615",
+        "heat": "102万热度",
+        "answer_count": 100,
+        "follower_count": 234
+    },
+    {
+        "rank": 17,
+        "id": "1935715188138566996",
+        "title": "关于文物中的「马」，你印象最深刻的是哪一个？",
+        "url": "https://www.zhihu.com/question/1935715188138566996",
+        "heat": "102万热度",
+        "answer_count": 36,
+        "follower_count": 52
+    },
+    {
+        "rank": 18,
+        "id": "2009262722156622270",
+        "title": "《镖人：风起大漠》票房超《射雕》成武侠片冠军，这对于武侠电影市场意味着什么？",
+        "url": "https://www.zhihu.com/question/2009262722156622270",
+        "heat": "102万热度",
+        "answer_count": 42,
+        "follower_count": 61
+    },
+    {
+        "rank": 19,
+        "id": "521184511",
+        "title": "为什么美国花滑主力基本上都是华裔，但中国花滑男女单的成绩却很一般？",
+        "url": "https://www.zhihu.com/question/521184511",
+        "heat": "98万热度",
+        "answer_count": 49,
+        "follower_count": 106
+    },
+    {
+        "rank": 20,
+        "id": "2009625522561365378",
+        "title": "12306 凌晨通知候补成功，网友睡醒已错过发车，1700 元车票作废，如何看待此事？该如何避免？",
+        "url": "https://www.zhihu.com/question/2009625522561365378",
+        "heat": "96万热度",
+        "answer_count": 261,
+        "follower_count": 555
+    }
+]
